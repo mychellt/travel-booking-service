97de2ae196c9e94ff453f2c94a3ba92a3cc658c5
Mychell Teixeira <mychellt@gmail.com>
2024-06-09 22:25:13 -0300

    fix: fixing mapper.

--- travel-booking-persistence/src/main/java/gft/challenge/travel/booking/persistence/adapters/TravelRepositoryAdapter.java
+++ travel-booking-persistence/src/main/java/gft/challenge/travel/booking/persistence/adapters/TravelRepositoryAdapter.java
@@ -3,7 +3,6 @@ package gft.challenge.travel.booking.persistence.adapters;
 import gft.challenge.travel.booking.core.persistence.TravelRepositoryPort;
 import gft.challenge.travel.booking.domain.Travel;
 import gft.challenge.travel.booking.persistence.mappers.PersistenceEntityMapper;
-import gft.challenge.travel.booking.persistence.model.TravelEntity;
 import gft.challenge.travel.booking.persistence.repository.TravelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -23,7 +22,6 @@ public class TravelRepositoryAdapter implements TravelRepositoryPort {
 
   @Override
   public Travel save(Travel obj) {
-    final TravelEntity travel = mapper.from(obj);
-    return mapper.from(repository.save(travel));
+    return mapper.from(repository.save(mapper.from(obj)));
   }
 }
--- travel-booking-persistence/src/main/java/gft/challenge/travel/booking/persistence/mappers/PersistenceEntityMapper.java
+++ travel-booking-persistence/src/main/java/gft/challenge/travel/booking/persistence/mappers/PersistenceEntityMapper.java
@@ -4,6 +4,8 @@ import gft.challenge.travel.booking.domain.Travel;
 import gft.challenge.travel.booking.persistence.model.TravelEntity;
 import org.mapstruct.*;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +15,10 @@ public interface PersistenceEntityMapper {
   @Mapping(source = "customerEmail", target = "customer.email")
   @Mapping(source = "customerName", target = "customer.name")
   @Mapping(source = "customerPhone", target = "customer.phone")
+  @Mapping(source = "flightResevationDetails", target = "flight")
+  @Mapping(source = "hotelResevationDetails", target = "hotel")
+  @Mapping(source = "carResevationDetails", target = "car")
+  @Mapping(source = "flightResevationDetails.seats", target = "flight.seats", qualifiedByName = "mapSeatsToIntegers")
   Travel from(final TravelEntity source);
 
   @Mapping(source = "customer.phone", target = "customerPhone")
@@ -31,6 +37,17 @@ public interface PersistenceEntityMapper {
         .map(String::valueOf)
         .collect(Collectors.joining(","));
   }
+
+  @Named("mapSeatsToIntegers")
+  default List<Integer> mapSeatsToIntegers(String seats) {
+    if (seats == null || seats.isEmpty()) {
+      return Collections.emptyList();
+    }
+    return Arrays.stream(seats.split(","))
+        .map(Integer::parseInt)
+        .toList();
+  }
+
   @AfterMapping
   default void after(@MappingTarget TravelEntity entity) {
     if (entity.getCarResevationDetails() != null) {
