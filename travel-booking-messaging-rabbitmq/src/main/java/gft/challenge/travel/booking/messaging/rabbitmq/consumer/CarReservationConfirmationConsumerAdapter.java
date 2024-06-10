37924de198db7c32fc85843511fa63937e3fa835
Mychell Teixeira <mychellt@gmail.com>
2024-06-10 09:21:21 -0300

    feat: confirmation for car reservation.

--- travel-booking-messaging-rabbitmq/src/main/java/gft/challenge/travel/booking/messaging/rabbitmq/consumer/CarReservationConfirmationConsumerAdapter.java
+++ travel-booking-messaging-rabbitmq/src/main/java/gft/challenge/travel/booking/messaging/rabbitmq/consumer/CarReservationConfirmationConsumerAdapter.java
@@ -1,14 +1,14 @@
 package gft.challenge.travel.booking.messaging.rabbitmq.consumer;
 
+import gft.challenge.travel.booking.messaging.rabbitmq.model.CarReservationConfirmationMessage;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CarReservationConfirmationConsumerAdapter implements CarReservationConfirmationConsumerPort {
+public class CarReservationConfirmationConsumerAdapter {
 
-  @Override
   @RabbitListener(queues = "${app.messaging.car-reservation-confirmation-queue}")
-  public void receive(final String message) {
+  public void receive(final CarReservationConfirmationMessage message) {
 
   }
 }
--- travel-booking-messaging-rabbitmq/src/main/java/gft/challenge/travel/booking/messaging/rabbitmq/consumer/FlightReservationConfirmationConsumerAdapter.java
+++ travel-booking-messaging-rabbitmq/src/main/java/gft/challenge/travel/booking/messaging/rabbitmq/consumer/FlightReservationConfirmationConsumerAdapter.java
@@ -4,9 +4,8 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FlightReservationConfirmationConsumerAdapter implements FlightReservationConfirmationConsumerPort {
+public class FlightReservationConfirmationConsumerAdapter {
 
-  @Override
   @RabbitListener(queues = "${app.messaging.flight-reservation-confirmation-queue}")
   public void receive(final String message) {
 
